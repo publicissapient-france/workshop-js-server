e6cab705579f43fb2154d954e99cddf530f16df8
yamsellem <amsellem.yves@gmail.com>
2013-05-19 11:25:57 +0200

    add naive cors impl

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express');
-    app = express();
+    app = express(),
+    allowCrossDomain = function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        next();
+    }
 
 app.use(express.static(__dirname));
 app.use(express.bodyParser());
+app.use(allowCrossDomain);
 
 app.get('/logs', function(req, res) {
     res.send(require('./data.json'));
